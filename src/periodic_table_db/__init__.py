592a959f1ebb0e44491146472d304d4bdca65694
Michael Wharmby <mtwharmby@gmail.com>
2024-05-09 15:55:20 +0200

    Restructures project with src directory.
    Version bump to 0.2.0. Updates pyproject.toml to fit new structure.

--- src/periodic_table_db/__init__.py
+++ src/periodic_table_db/__init__.py
@@ -1,4 +1,4 @@
-from periodic_table_db.shared import (
+from .shared import (
     Element, AtomicWeight,
     ATOMIC_NR, ELEM_SYMBOL, ELEM_NAME, ELEM_WEIGHT_ID, AT_WEIGHT,
     AT_WEIGHT_ESD, AT_WEIGHT_MIN, AT_WEIGHT_MAX, AT_WEIGHT_TYPE_ID,
@@ -12,4 +12,4 @@ __all__ = [
     WEIGHT_TYPE_NONE, WEIGHT_TYPE_INTERVAL, WEIGHT_TYPE_REPORTED
 ]
 
-VERSION = "0.1.1"
+VERSION = "0.2.0"
