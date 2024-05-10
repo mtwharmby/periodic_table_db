82250d3862a7cf0921a444134a66e9c74ae5c6d3
Michael Wharmby <mtwharmby@gmail.com>
2024-05-10 23:59:41 +0200

    Initial commit of groups data

---
+++ src/periodic_table_db/extended/data/group_data.py
@@ -0,0 +1,95 @@
+# TODO Add citations!
+
+groups = [
+    {
+        "number": 1,
+        "label_eu": "IA",
+        "label_us": "IA",
+        "name": "Alkali Metals"
+    }, {
+        "number": 2,
+        "label_eu": "IIA",
+        "label_us": "IIA",
+        "name": "Alkaline Earth Metals"
+    }, {
+        "number": 3,
+        "label_eu": "IIIA",
+        "label_us": "IIIB",
+        "name": None
+    }, {
+        "number": 4,
+        "label_eu": "IVA",
+        "label_us": "IVB",
+        "name": None
+    }, {
+        "number": 5,
+        "label_eu": "VA",
+        "label_us": "VB",
+        "name": None
+    }, {
+        "number": 6,
+        "label_eu": "VIA",
+        "label_us": "VIB",
+        "name": None
+    }, {
+        "number": 7,
+        "label_eu": "VIIA",
+        "label_us": "VIIB",
+        "name": None
+    }, {
+        "number": 8,
+        "label_eu": "VIIIA",
+        "label_us": "VIIIB",
+        "name": None
+    }, {
+        "number": 9,
+        "label_eu": "VIIIA",
+        "label_us": "VIIIB",
+        "name": None
+    }, {
+        "number": 10,
+        "label_eu": "VIIIA",
+        "label_us": "VIIIB",
+        "name": None
+    }, {
+        "number": 11,
+        "label_eu": "IB",
+        "label_us": "IB",
+        "name": "Coinage Metals"
+    }, {
+        "number": 12,
+        "label_eu": "IIB",
+        "label_us": "IIB",
+        "name": None
+    }, {
+        "number": 13,
+        "label_eu": "IIIB",
+        "label_us": "IIIA",
+        "name": None
+    }, {
+        "number": 14,
+        "label_eu": "IVB",
+        "label_us": "IVA",
+        "name": None
+    }, {
+        "number": 15,
+        "label_eu": "VB",
+        "label_us": "VA",
+        "name": "Pnictogens"
+    }, {
+        "number": 16,
+        "label_eu": "VIB",
+        "label_us": "VIA",
+        "name": "Chalcogens"
+    }, {
+        "number": 17,
+        "label_eu": "VIIB",
+        "label_us": "VIIA",
+        "name": "Halogens"
+    }, {
+        "number": 18,
+        "label_eu": "VIIIB",
+        "label_us": "VIIIA",
+        "name": "Noble Gases"
+    }
+]
