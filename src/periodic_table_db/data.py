952d98d199bafff894ac177c028e7848aed5ae3e
Michael Wharmby <mtwharmby@gmail.com>
2024-05-12 09:53:15 +0200

    Moves atomic_weight_types data into data module

---
+++ src/periodic_table_db/data.py
@@ -0,0 +1,31 @@
+from . import (
+    WEIGHT_TYPE_NONE, WEIGHT_TYPE_INTERVAL, WEIGHT_TYPE_REPORTED
+)
+
+atomic_weight_types = [
+    {
+        "name": WEIGHT_TYPE_NONE,
+        "method": "none",
+        "reason": "No stable isotope(s) with characteristic isotopic "
+                  "composition exist. A standard atomic weight cannot be "
+                  "determined."
+    },
+    {
+        "name": WEIGHT_TYPE_INTERVAL,
+        "method": "calculated",
+        "reason": "CIAAW expresses atomic weight as an interval intended to "
+                  "encompass all \"normal\" materials. Atomic weight has been "
+                  "calculated as the midpoint of the interval; uncertainty is "
+                  "calculated as half the difference between the maximum and "
+                  "minimum of the interval."
+    },
+    {
+        "name": WEIGHT_TYPE_REPORTED,
+        "method": "tabulated",
+        "reason": "CIAAW provides a single value for the atomic weight with "
+                  "an uncertainty. The quoted atomic weight is representative "
+                  "of the population of a sample of atoms of the element. A "
+                  "minimum and maximum weight have been calculated from the "
+                  "uncertainty."
+    }
+]
--- src/periodic_table_db/database.py
+++ src/periodic_table_db/database.py
@@ -5,8 +5,9 @@ from sqlalchemy import (
 )
 
 from . import (
-    Element, WEIGHT_TYPE_NONE, WEIGHT_TYPE_INTERVAL, WEIGHT_TYPE_REPORTED
+    Element, WEIGHT_TYPE_NONE
 )
+from .data import atomic_weight_types as at_weight_values
 from .schema import (
     element_table, atomic_weight_table, atomic_weight_type_table
 )
@@ -39,36 +40,6 @@ class PeriodicTableDBBase:
         """
         Create the constants in the atomic weight type table.
         """
-        # TODO Move these to new data module
-        at_weight_values = [
-            {
-                "name": WEIGHT_TYPE_NONE,
-                "method": "none",
-                "reason": "No stable isotope(s) with characteristic isotopic "
-                          "composition exist. A standard atomic weight cannot "
-                          "be determined."
-            },
-            {
-                "name": WEIGHT_TYPE_INTERVAL,
-                "method": "calculated",
-                "reason": "CIAAW expresses atomic weight as an interval "
-                          "intended to encompass all \"normal\" materials. "
-                          "Atomic weight has been calculated as the midpoint "
-                          "of the interval; uncertainty is calculated as half "
-                          "the difference between the maximum and minimum of "
-                          "the interval."
-            },
-            {
-                "name": WEIGHT_TYPE_REPORTED,
-                "method": "tabulated",
-                "reason": "CIAAW provides a single value for the atomic "
-                          "weight with an uncertainty. The quoted atomic "
-                          "weight is representative of the population of a "
-                          "sample of atoms of the element. A minimum and "
-                          "maximum weight have been calculated from the "
-                          "uncertainty."
-            }
-        ]
         logger.info(
             f"Adding weight types to {self.atomic_weight_type.name} table."
         )
