f8481a3dfe00d1a14c53f34c17dbb7045e8948e6
Michael Wharmby <mtwharmby@gmail.com>
2024-05-18 23:12:45 +0200

    Initial commit of add_electronic_structure_data

--- src/periodic_table_db/extended/database.py
+++ src/periodic_table_db/extended/database.py
@@ -1,12 +1,20 @@
+from contextlib import nullcontext
 import logging
 
-from sqlalchemy import Engine, MetaData, insert
+from sqlalchemy import (
+    Engine, MetaData, Connection, insert, select, bindparam, update,
+)
 
 from .. import PeriodicTableDBBase
+from ..shared import (
+    ATOMIC_NR, E_SHELL_STRUCT, E_SUB_SHELL_STRUCT, PERIOD, GROUP, BLOCK,
+    BLOCK_ID
+)
 from .data.group_data import groups as group_values
 from .schema import (
     period_table, group_table, block_table, label_table, label_to_element_table
 )
+from .data import Atom
 
 logger = logging.getLogger(__name__)
 
@@ -24,13 +32,66 @@ class PeriodicTableDB(PeriodicTableDBBase):
             self.metadata_obj, **kwargs
         )
 
-    def add_groups(self):
-        with self.engine.connect() as conn:
+    def add_groups(self, conn: Connection = None):
+        with (nullcontext(conn) if conn else self.connect()) as conn:
             logger.info(
                 f"Adding group numbers, names and labels to {self.group.name} "
                 "table."
             )
-            conn.execute(
-                insert(self.group), group_values
+            conn.execute(insert(self.group), group_values)
+            conn.commit()
+
+    def add_electronic_structure_data(self, conn: Connection = None):
+        with (nullcontext(conn) if conn else self.connect()) as conn:
+            all_atomic_nrs = self.get_atomic_nrs(conn=conn)
+
+            electronic_configs = [
+                Atom(nr).dict() for nr in all_atomic_nrs
+            ]
+
+            # Update entries in the Element table
+            elem_values = [
+                {
+                    # Following needs to be different name to the column to
+                    # avoid collision with bindparameter
+                    "atomic_nr": config[ATOMIC_NR],
+                    PERIOD: config[PERIOD],
+                    GROUP: config[GROUP],
+                    BLOCK: config[BLOCK]
+                 } for config in electronic_configs
+            ]
+            block_subq = (
+                select(self.block.c[BLOCK_ID])
+                .where(self.block.c[BLOCK] == bindparam(BLOCK))
+                .scalar_subquery()
             )
+            elem_update_stmt = (
+                update(self.element)
+                .where(self.element.c[ATOMIC_NR] == bindparam("atomic_nr"))
+                .values(block_id=block_subq)
+            )
+            logger.info(f"Updating {len(elem_values)} entries in "
+                        f"{self.element.name} table with electronic "
+                        "configuration")
+            conn.execute(elem_update_stmt, elem_values)
+
+            # Update the entries in the Ion table
+            ion_values = [
+                {
+                    "atomic_nr": config[ATOMIC_NR],
+                    E_SHELL_STRUCT: config[E_SHELL_STRUCT],
+                    E_SUB_SHELL_STRUCT: config[E_SUB_SHELL_STRUCT]
+                } for config in electronic_configs
+            ]
+
+            ions_update_stmt = (
+                update(self.ion)
+                .where(self.ion.c[ATOMIC_NR] == bindparam("atomic_nr"))
+            )
+            logger.info(f"Updating {len(ion_values)} entries in "
+                        f"{self.ion.name} table with electronic configuration."
+                        )
+            conn.execute(ions_update_stmt, ion_values)
+
+            # Element and Ion table statements worked, so commit the changes
             conn.commit()
