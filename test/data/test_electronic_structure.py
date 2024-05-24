f99f4c5a568a4686f7a1a49ac08cbbf74b963a58
Michael Wharmby <mtwharmby@gmail.com>
2024-05-24 13:31:53 +0200

    Adds additional tests of electronic_structure module

--- test/data/test_electronic_structure.py
+++ test/data/test_electronic_structure.py
@@ -37,6 +37,61 @@ class TestSubShell:
         assert rest == {"electrons": 2, "sequence": [(3, 2)]}
         assert sub_shell.is_full
 
+    def test_add_electrons_noargs(self):
+        sub_shell = SubShell(3, 2)
+        assert sub_shell.electrons == 0
+
+        rest = sub_shell.add_electrons()
+        assert rest == 0
+        assert sub_shell.electrons == 1
+
+    def test_add_electrons(self):
+        sub_shell = SubShell(3, 2)
+        assert sub_shell.electrons == 0
+
+        rest = sub_shell.add_electrons(6)
+        assert rest == 0
+        assert sub_shell.electrons == 6
+        assert sub_shell.orbitals == [2, 1, 1, 1, 1]
+
+        rest = sub_shell.add_electrons(6)
+        assert rest == 2
+        assert sub_shell.electrons == 10
+        assert sub_shell.orbitals == [2, 2, 2, 2, 2]
+
+    def test_remove_electrons_noargs(self):
+        sub_shell = SubShell(3, 2)
+        rest = sub_shell.add_electrons(9)
+
+        sub_shell.remove_electrons()
+        assert rest == 0
+        assert sub_shell.electrons == 8
+        assert sub_shell.orbitals == [2, 2, 2, 1, 1]
+
+    def test_remove_electrons(self):
+        sub_shell = SubShell(3, 2)
+        sub_shell.add_electrons(9)
+
+        rest = sub_shell.remove_electrons(2)
+        assert rest == 0
+        assert sub_shell.electrons == 7
+        assert sub_shell.orbitals == [2, 2, 1, 1, 1]
+
+        rest = sub_shell.remove_electrons(9)
+        assert rest == -2
+        assert sub_shell.electrons == 0
+
+    def test_str(self):
+        sub_shell = SubShell(3, 2)
+        sub_shell.add_electrons(6)
+        assert str(sub_shell) == "3d^{6}"
+
+    def test_from_sub_shell_struct(self):
+        sub_shell = SubShell.from_sub_shell_structure("5f^{9}")
+        assert sub_shell._pqn == 5
+        assert sub_shell._aqn == 3
+        assert sub_shell.electrons == 9
+
 
 class TestAtom:
 
@@ -70,9 +125,26 @@ class TestAtom:
     @pytest.mark.parametrize(
         "at_nr, sub_shell_struct",
         [(2, "1s^{2}"),
-         (29, "1s^{2}.2s^{2}.2p^{6}.3s^{2}.3p^{6}.3d^{10}.4s^{1}")
+         # Note: this is the wrong ground state or Cu, but is correct following
+         # the rules.
+         (29, "1s^{2}.2s^{2}.2p^{6}.3s^{2}.3p^{6}.3d^{9}.4s^{2}")
          ]
     )
     def test_sub_shell_structure(self, at_nr, sub_shell_struct):
         at = Atom(at_nr)
+        assert at.sub_shell_structure == sub_shell_struct
+
+    @pytest.mark.parametrize(
+        "at_nr, correction, electrons, sub_shell_struct",
+        [(29, "3d^{10}.4s^{1}", 29, "1s^{2}.2s^{2}.2p^{6}.3s^{2}.3p^{6}."
+                                    "3d^{10}.4s^{1}")
+         ]
+    )
+    def test_correct_orbital_filling(
+        self, at_nr, correction, electrons, sub_shell_struct
+    ):
+        at = Atom(at_nr)
+        assert at.electrons == electrons
+        at.correct_orbital_filling(correction)
+        assert at.electrons == electrons
         assert at.sub_shell_structure == sub_shell_struct
