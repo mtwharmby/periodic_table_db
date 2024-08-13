4f0788eee7c8b952000251598c65491d07190086
Michael Wharmby <mtwharmby@gmail.com>
2024-08-14 00:44:59 +0200

    Refactors shared
    Moves Ion to package base shared. Adds new factory and parser for reading ions.

--- src/periodic_table_db/builder/shared.py
+++ src/periodic_table_db/builder/shared.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass, asdict
-import re
 
 from ..shared import (  # noqa: F401
     ATOMIC_NR, ELEM_SYMBOL, AT_WEIGHT, WEIGHT_TYPE_NONE, WEIGHT_TYPE_INTERVAL,
@@ -27,51 +26,3 @@ class Element:
     weight: AtomicWeight
 
     dict = asdict
-
-
-@dataclass
-class Ion:
-    element_symbol: str
-    charge: int
-    atomic_number: int | None = None
-    symbol: str | None = None
-
-    def dict(self) -> dict[str, int | str]:
-        if not self.symbol:
-            if self.charge > 0:
-                self.symbol = f"{self.element_symbol}{self.charge}+"
-            elif self.charge < 0:
-                self.symbol = f"{self.element_symbol}{self.charge}-"
-            else:
-                self.symbol = f"{self.element_symbol}"
-
-        d = asdict(self)
-        del d["element_symbol"]
-        return d
-
-
-ion_symbol_re = re.compile(r"(^[A-Z][a-z]?)(\d*)([+-]?)")
-
-
-def make_ion_for_symbol(symbol: str, atomic_nr: int = None) -> Ion:
-    symbol_parts = ion_symbol_re.search(symbol)
-    assert symbol_parts is not None
-
-    elem_symbol = symbol_parts.group(1)
-    if symbol_parts.group(2):
-        charge = int(symbol_parts.group(2))
-        if symbol_parts.group(3) == "-":
-            charge = -charge
-    elif symbol_parts.group(3) == "+":
-        charge = 1
-    elif symbol_parts.group(3) == "-":
-        charge = -1
-    else:
-        charge = 0
-
-    return Ion(
-        element_symbol=elem_symbol,
-        charge=charge,
-        symbol=symbol,
-        atomic_number=atomic_nr
-    )
--- src/periodic_table_db/shared.py
+++ src/periodic_table_db/shared.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass, asdict
+import re
+
 ATOMIC_NR = "atomic_number"
 ELEM_SYMBOL = "symbol"
 
@@ -19,3 +22,72 @@ BLOCK = "block"
 BLOCK_ID = "id"
 LABEL = "name"
 LABEL_ID = "label_id"
+
+TABLE_NAMES = [
+    "Element", "AtomicWeight", "AtomicWeightType", "Ion"
+]
+
+TABLE_NAMES_EXTENDED = [
+    "Period", "Group", "Block", "Label", "ElementLabel"
+]
+
+
+@dataclass
+class Ion:
+    symbol: str
+    charge: int
+    atomic_number: int | None = None
+
+    dict = asdict
+
+    @property
+    def element_symbol(self):
+        symbol_parts = ion_symbol_re.match(self.symbol)
+        return symbol_parts.group(1)
+
+
+ion_symbol_re = re.compile(
+    r"(^[A-Z][a-z]?)(?:(?:(\d+)?([+-])(\3)*)?|(?:\((I+)\))?)$"
+)
+
+
+def parse_ion_symbol(symbol: str, atomic_nr: int = None):
+    symbol_parts = ion_symbol_re.match(symbol)
+    if symbol_parts is None:
+        raise RuntimeError(f"Cannot parse ion symbol {symbol}")
+
+    elem_symbol = symbol_parts.group(1)
+    charge = 0
+    if symbol_parts.group(2) is not None:
+        charge = int(symbol_parts.group(2))
+        if symbol_parts.group(3) == "+":
+            pass
+        elif symbol_parts.group(3) == "-":
+            charge = -charge
+        else:
+            raise RuntimeError(f"Cannot parse ion symbol {symbol}")
+    elif symbol_parts.group(3) is not None:
+        charge = len(symbol_parts.group(3))
+        if "-" in symbol_parts:
+            charge = -charge
+
+    return {
+        "element_symbol": elem_symbol,
+        "charge": charge,
+        "atomic_number": atomic_nr
+    }
+
+
+def ion_factory(element_symbol, charge, atomic_number=None):
+    if charge > 0:
+        symbol = f"{element_symbol}{abs(charge)}+"
+    elif charge < 0:
+        symbol = f"{element_symbol}{abs(charge)}-"
+    else:
+        symbol = f"{element_symbol}"
+
+    return Ion(
+        symbol=symbol,
+        charge=charge,
+        atomic_number=atomic_number
+    )
