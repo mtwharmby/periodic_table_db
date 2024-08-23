c98c2c68abae6190b5a9ef8bb73f57de93bb2d99
Michael Wharmby <mtwharmby@gmail.com>
2024-08-23 12:26:44 +0200

    Adds exports needed by DiffrnDB to module base

--- src/periodic_table_db/__init__.py
+++ src/periodic_table_db/__init__.py
@@ -1,8 +1,10 @@
 from .dbconnector import DBConnector
 from .dbapi import PeriodicTableDBAPI
+from .shared import Ion, parse_ion_symbol
 
 __all__ = [
-    DBConnector, PeriodicTableDBAPI
+    DBConnector, PeriodicTableDBAPI,
+    Ion, parse_ion_symbol
 ]
 
 VERSION = "0.2.4"
